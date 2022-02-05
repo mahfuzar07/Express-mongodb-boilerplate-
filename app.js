0abfc03f358c21112ed5d9dc149762a6dbae27ca
Your NameMahfuzar <58438084+mahfuzar07@users.noreply.github.com>
2022-02-05 14:50:33 +0600

    host

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 dotenv.config();
 // ========request connection==========
 app.use(express.json());
 // ========database connection==========
+
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
-    console.log("dtabase connection successfull");
+    console.log('dtabase connection successfull');
   })
   .catch((err) => console.log(err));
 
 // ========routes==========
-const homeroute = require("./routes/home");
+const homeroute = require('./routes/home');
 app.use(homeroute);
 
 // ========set static folder==========
-app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 // ========server listener==========
-app.listen(process.env.PORT, () => {
-  console.log(`surver running successfull on  port ${process.env.PORT}`);
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+app.listen(PORT, () => {
+  console.log(`server is running successfully at http://${HOST}:${PORT}`);
 });
